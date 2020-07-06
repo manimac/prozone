3a45775ef83dcc7582438fccdf9b623c2bd36e27
Venkat121mca <venkat99mca@gmail.com>
2020-07-07 00:36:43 +0530

    application status update completed

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -3,6 +3,7 @@ import { HttpRequestService } from 'app/services/http-request/http-request.servi
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-application-form',
@@ -12,6 +13,9 @@ import { MatSort } from '@angular/material/sort';
 export class ApplicationFormComponent implements OnInit {
 
   dataLists: any = [];
+  applicationData: any = [];
+  categoryData: any = [];
+  subCategoryData: any = [];
   showLoader = false;
 
   displayedColumns: string[] = ['email', 'mobile'];
@@ -42,11 +46,14 @@ export class ApplicationFormComponent implements OnInit {
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  constructor(private http: HttpRequestService) { 
+  constructor(private http: HttpRequestService, private router: Router) { 
   }
 
   ngOnInit(): void {
     this.loadUsers();
+    this.loadCategories();
+    this.loadSubCategories();
+    this.getApplicationList();
   }
 
   loadUsers() {
@@ -64,11 +71,78 @@ export class ApplicationFormComponent implements OnInit {
     )
   }
 
+  getApplicationList() {
+    this.showLoader = true;
+    this.http.get('application/get').subscribe(
+      (response) => {
+        this.applicationData = response;
+        this.applicationData.forEach(element => {
+          if(element.categoryID && this.categoryData.length > 0) {
+            this.categoryData.forEach(element1 => {
+              if(element1._id == element.categoryID)
+                element.categoryName = element1.name_english;
+            });
+          }
+          if(element.subCategoryID && this.subCategoryData.length > 0) {
+            this.subCategoryData.forEach(element1 => {
+              if(element1._id == element.subCategoryID)
+                element.subCategoryName = element1.name_english;
+            });
+          }
+          // if(element.userID && this.dataLists.length > 0){
+          //   this.dataLists.forEach(element1 => {
+          //     if(element1._id == element.userID)
+          //       element.userName = element1.name;
+          //   });
+          // }
+        });
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
+  loadCategories() {
+    this.showLoader = true;
+    this.http.get('api/category/get-category').subscribe(
+      (response) => {
+        this.categoryData = response;
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
+  loadSubCategories() {
+    this.showLoader = true;
+    this.http.get('api/category/get-subcategory').subscribe(
+      (response) => {
+        this.subCategoryData = response;
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
   loadMaterial(){
     this.dataSource = new MatTableDataSource<PeriodicElement>(this.dataLists);
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
   }
+
+  selectApplication(id){
+    this.router.navigate(['/application-view'], {queryParams: {applicationId: id}})
+  }
+
 }
 
 export interface PeriodicElement {
--- src/app/application-view/application-view.component.ts
+++ src/app/application-view/application-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpRequestService } from 'app/services/http-request/http-request.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-application-view',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationViewComponent implements OnInit {
 
+  applicationData: any = [];
+  categoryData: any = [];
+  subCategoryData: any = [];
+  statusHistory: any = [];
+  showLoader = false;
+  statusName: any = "123";
+  applicationId : any;
+
   status = [
-    {
+  {
       id: 1,
       name: 'In progress'
     },
@@ -25,9 +35,101 @@ export class ApplicationViewComponent implements OnInit {
       name: 'Rejected'
     }
   ]
-  constructor() { }
+
+  constructor(private http: HttpRequestService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.applicationId= params['applicationId'];
+    });
+
+    this.loadCategories();
+    this.loadSubCategories();
+    this.getApplicationList();
+  }
+
+  getApplicationList() {
+    this.showLoader = true;
+    
+    this.http.get('application/view?applicationId='+this.applicationId).subscribe(
+      (response) => {
+        this.applicationData = response;
+        if(this.applicationData.length > 0) {
+          this.statusHistory = this.applicationData[0].status;
+          if(this.applicationData[0].categoryID && this.categoryData.length > 0) {
+            this.categoryData.forEach(element1 => {
+              if(element1._id == this.applicationData[0].categoryID)
+              this.applicationData[0].categoryName = element1.name_english;
+            });
+          }
+          if(this.applicationData[0].subCategoryID && this.subCategoryData.length > 0) {
+            this.subCategoryData.forEach(element1 => {
+              if(element1._id == this.applicationData[0].subCategoryID)
+              this.applicationData[0].subCategoryName = element1.name_english;
+            });
+          }
+        }
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
+  loadCategories() {
+    this.showLoader = true;
+    this.http.get('api/category/get-category').subscribe(
+      (response) => {
+        this.categoryData = response;
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
+  loadSubCategories() {
+    this.showLoader = true;
+    this.http.get('api/category/get-subcategory').subscribe(
+      (response) => {
+        this.subCategoryData = response;
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    )
+  }
+
+  save(){
+    this.applicationData[0].status = (this.applicationData[0].status && this.applicationData[0].status.length > 0) ? this.applicationData[0].status : [];
+    this.applicationData[0].status.push(this.statusName);
+    this.statusHistory = this.applicationData[0].status;
+    let data = this.applicationData[0];
+    delete data.categoryName;
+    delete data.subCategoryName;
+    this.http.post('application/create', data).subscribe(
+      (response) => {
+        console.log("respone :"+response)
+        this.showLoader = false;       
+      },
+      (error) => {
+        this.showLoader = false;
+        this.http.exceptionHandling(error);
+      }
+    );
+  }
+
+  changeStatus(val){
+    this.status.forEach(element => {
+      if(element.id == val.value)
+        this.statusName = element.name;
+    });
   }
 
 }
